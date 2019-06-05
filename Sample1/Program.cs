eb76ca6214c8380be6e582a2085c85c2cc85d673
董煜 <smock130@msn.com>
2019-06-05 16:12:50 +0800

    ~

--- Sample1/Program.cs
+++ Sample1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SmockAspNetLib.Infrastructure.Extensions;
+using System;
 using System.Collections.Generic;
 
 namespace Samples.Sample1
@@ -9,25 +10,32 @@ namespace Samples.Sample1
         {
             Console.WriteLine("Hello World!");
 
-            List<int> list = new List<int>();
+            DateTime dateTime = DateTime.Now;
+            DateTime dateTime2 = DateTime.UtcNow;
 
-            int i = 10000;
-            int j = 2;
-            while (i>0)
-            {
-                Random rd = new Random();
-                var result = rd.Next(1, j+1);
-                Console.Write(result + ",");
-                list.Add(result);
-                i--;
-            }
+            var d1 = dateTime.ToUnix(true);
+            var d2 = dateTime2.ToUnix(true);
 
-            Console.WriteLine("-------------");
 
-            for(int a = 0; a <= j; a++)
-            {
-                Console.WriteLine(a+"：" + list.FindAll(e => e == a).Count);
-            }
+            //List<int> list = new List<int>();
+
+            //int i = 10000;
+            //int j = 2;
+            //while (i>0)
+            //{
+            //    Random rd = new Random();
+            //    var result = rd.Next(1, j+1);
+            //    Console.Write(result + ",");
+            //    list.Add(result);
+            //    i--;
+            //}
+
+            //Console.WriteLine("-------------");
+
+            //for(int a = 0; a <= j; a++)
+            //{
+            //    Console.WriteLine(a+"：" + list.FindAll(e => e == a).Count);
+            //}
 
             Console.ReadKey();
         }
---
+++ SmockAspNetLib.Infrastructure/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmockAspNetLib.Infrastructure.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static long ToUnix(this DateTime dateTime, bool isMilliseconds = false)
+        {
+            DateTime timeStamp = new DateTime(1970, 1, 1); //得到1970年的时间戳
+            if (isMilliseconds)
+            {
+                long a = (dateTime.ToUniversalTime().Ticks - timeStamp.Ticks) / 10000; //注意这里有时区问题，用now就要减掉8个小时
+                return a;
+            }
+            else
+            {
+                long a = (dateTime.ToUniversalTime().Ticks - timeStamp.Ticks) / 10000000; //注意这里有时区问题，用now就要减掉8个小时
+                return a;
+            }
+            
+            
+        }
+    }
+}
